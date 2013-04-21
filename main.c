2b829f3bd1a2b60ee4edbd943baa53567aae140e
Artyom Poptsov <poptsov.artyom@gmail.com>
2013-04-21 13:57:08 +0400

    main.c: Print help if the user passes '-h' to the program.

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+void print_help(char *program_name)
 {
+	printf("Usage: %s [ -h ]\n", program_name);
+}
+
+int main(int argc, char **argv)
+{
+	if ((argc > 1) && (! strcmp(argv[1], "-h"))) {
+		print_help(argv[0]);
+		exit(0);
+	}
+
 	puts("Hello World! \n");
 
 	return 0;
